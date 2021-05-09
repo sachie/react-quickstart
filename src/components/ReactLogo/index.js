dd705bba81541bc639ceaa407f1992d8d34436b0
Sachindra Ariyasinghe <sachindra.ariyasinghe@gmail.com>
2021-05-09 17:16:44 +0530

    Add tests and coverage scripts. Update github workflow.

--- src/components/ReactLogo/index.js
+++ src/components/ReactLogo/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Spinner from 'assets/icons/logo.svg';
+import Logo from 'assets/icons/logo.svg';
 import styles from './reactLogo.module.scss';
 
 const ReactLogo = () => {
-  return <img className={styles.logo} src={Spinner} alt="Loader" />;
+  return <img className={styles.logo} src={Logo} alt="Logo" />;
 };
 
 export default React.memo(ReactLogo);
